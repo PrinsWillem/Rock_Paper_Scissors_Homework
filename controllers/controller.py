162e58896612fd5e10abb3c2b4c81740ab9ca18d
Willem Prins <willemprins@Willems-MacBook-Pro.local>
2021-09-20 06:26:40 +0100

    both working player vs player and computer vs player

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,16 +5,31 @@ from models.player import Player
 from models.game import Game
 import random
 
+@app.route('/')
+def welcome():
+    return render_template("welcome.html", title="Home")
+
+@app.route('/game-player')
+def index():
+    return render_template("player1.html", title="player1_choice")
+
+@app.route('/<choice_1>')
+def player_choice_vs_player(choice_1):
+    return render_template("player2.html", title="player2_choice", choice_1=choice_1)
+
 @app.route('/<choice_1>/<choice_2>')
 def play_the_game(choice_1, choice_2):
     player_1 = Player("Player 1", choice_1)
     player_2 = Player("Player 2", choice_2)
     game = Game()
     winner = game.play(player_1, player_2)
-    return f"Player 1 chose: {player_1.choice}, Player 2 chose: {player_2.choice}. The winner is {winner}." 
+    return render_template("result.html", winner = winner)
 
+@app.route('/game-computer')
+def player_choice_vs_computer():
+    return render_template("player_vs_com.html", title="player1_choice")
 
-@app.route('/<choice>')
+@app.route('/game-computer/<choice>')
 def play_the_game_vs_computer(choice):
     player = Player("Player", choice)
     choice = ["rock", "paper", "scissors"]
@@ -22,7 +37,9 @@ def play_the_game_vs_computer(choice):
     computer = Player("Computer", computer_choice)
     game = Game()
     winner = game.play(player, computer)
-    return f"Player chose: {player.choice}, Computer chose: {computer.choice}. The winner is {winner}"
+    return render_template("result.html", winner = winner)
+
+
 
 
     
